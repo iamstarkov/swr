f37426b60daa94ec6eab0644bd6c0ee4150614e9
Vladimir Starkov <iamstarkov@gmail.com>
2020-01-14 23:51:16 +0100

    issue link fixed

--- examples/server-render/components/nav.js
+++ examples/server-render/components/nav.js
@@ -2,7 +2,7 @@ export default function Nav() {
   return (<>
     <nav>
       <h1>
-        <a href="https://github.com/zeit/swr/issues/178">zeit/swr#178 demo</a>
+        <a href="https://github.com/zeit/swr/issues/230">zeit/swr#230 demo</a>
       </h1>
       <a href="/demo">demo</a> and <a href="/workaround">workaround</a>
     </nav>
